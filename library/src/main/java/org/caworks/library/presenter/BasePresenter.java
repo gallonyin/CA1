49b09cf1ff2ebd1730f0e3225686ff0b517c94e5
Gallon-PC\Gallon <gallonyin@gmail.com>
2017-03-16 00:08:48 +0800

    Modify dependencies structure

---
+++ library/src/main/java/org/caworks/library/presenter/BasePresenter.java
@@ -0,0 +1,16 @@
+package org.caworks.library.presenter;
+
+/**
+ * presenter interface
+ * All presenter must implement this.
+ *
+ * Created by Gallon on 2017/3/15.
+ */
+
+public interface BasePresenter {
+
+    void subscribe();
+
+    void unSubscribe();
+
+}
